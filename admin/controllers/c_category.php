bf4866857e04c54cb540a30724cdd0f3696877cc
anhvt21134 <115914495+anhvt21134@users.noreply.github.com>
2022-12-08 19:44:05 +0700

    update category

--- admin/controllers/c_category.php
+++ admin/controllers/c_category.php
@@ -1,20 +1,42 @@
 <?php
 require('models/m_category.php');
 
-function hien_thi_ds_danhMuc(){
+function hien_thi_ds_danhMuc()
+{
     $danh_sach_danh_muc = danh_sach_danh_muc();
     include('view/category/all_category.php');
 }
 
-function them_moi_danh_muc(){
+function them_moi_danh_muc()
+{
+    if (isset($_POST['them_danhmuc'])) {
+        $ten_danhmuc = $_POST['ten_danhmuc'];
+        $mau_nen = $_POST['mau_nen'];
+        $icon = $_POST['icon'];
+
+        them_danh_muc($ten_danhmuc, $mau_nen, $icon);
+        // sql query for insert data
+
+        echo "<script>
+            window.location.href='index.php?url=danh_sach_danh_muc';
+        </script>";
+        // if (pdo_execute($sql)) {
+
+        //     header("Location:index.php?url=danh_sach_danh_muc");
+        // }
+    }
     include('view/category/add_category.php');
-    them_danh_muc();
 }
 
-function xoa_danh_muc(){
-    if(isset($_GET['category_id'])){
+function xoa_danh_muc()
+{
+    if (isset($_GET['category_id'])) {
         $ma_danhmuc = $_GET['category_id'];
         xoa_danhmuc($ma_danhmuc);
+
+        echo "<script>
+            window.location.href='index.php?url=danh_sach_danh_muc';
+        </script>";
     }
 }
 
@@ -27,7 +49,22 @@ function form_sua_danhmuc()
     include('view/category/edit_category.php');
 }
 
-function sua_danh_muc(){
-    sua_danhmuc();
+function sua_danh_muc()
+{
+    if (isset($_POST['them_danhmuc'])) {
+        $ma_danhmuc = $_POST['ma_danhmuc'];
+        $ten_danhmuc = $_POST['ten_danhmuc'];
+        $mau_nen = $_POST['mau_nen'];
+        $icon = $_POST['icon'];
+
+        // sql query for insert data
+        sua_danhmuc($ma_danhmuc, $ten_danhmuc, $mau_nen, $icon);
+        echo "<script>
+            window.location.href='index.php?url=danh_sach_danh_muc';
+        </script>";
+        // if (pdo_execute($sql)) {
+
+        //     header("Location:index.php?url=danh_sach_danh_muc");
+        // }
+    }
 }
-?>
--- admin/models/m_category.php
+++ admin/models/m_category.php
@@ -8,35 +8,16 @@ function danh_sach_danh_muc()
     return $danh_sach_danh_muc;
 }
 
-function them_danh_muc()
+function them_danh_muc($ten_danhmuc, $mau_nen, $icon)
 {
-    if (isset($_POST['them_danhmuc'])) {
-        $ten_danhmuc = $_POST['ten_danhmuc'];
-        $mau_nen = $_POST['mau_nen'];
-        $icon = $_POST['icon'];
-
-        // sql query for insert data
-        $sql = "INSERT INTO danhmuc (ten_danhmuc, mau_nen, icon)
-                VALUES ('$ten_danhmuc', '$mau_nen', '$icon')";
-        pdo_execute($sql);
-        echo "<script>
-            window.location.href='index.php?url=danh_sach_danh_muc';
-        </script>";
-        // if (pdo_execute($sql)) {
-
-        //     header("Location:index.php?url=danh_sach_danh_muc");
-        // }
-    }
-    // if (isset($msg)) echo $msg;
+    $sql = "INSERT INTO danhmuc (ten_danhmuc, mau_nen, icon) VALUES ('$ten_danhmuc', '$mau_nen', '$icon')";
+    pdo_execute($sql);
 }
 
 function xoa_danhmuc($ma_danhmuc)
 {
     $sql = "DELETE FROM danhmuc WHERE ma_danhmuc = '$ma_danhmuc' ";
     pdo_execute($sql);
-    echo "<script>
-            window.location.href='index.php?url=danh_sach_danh_muc';
-        </script>";
 }
 
 //function get category by category_id
@@ -47,26 +28,8 @@ function lay_danhmuc_theo_id($ma_danhmuc)
     return $danhmuc;
 }
 
-function sua_danhmuc()
+function sua_danhmuc($ma_danhmuc, $ten_danhmuc, $mau_nen, $icon)
 {
-    if (isset($_POST['them_danhmuc'])) {
-        $ma_danhmuc = $_POST['ma_danhmuc'];
-        $ten_danhmuc = $_POST['ten_danhmuc'];
-        $mau_nen = $_POST['mau_nen'];
-        $icon = $_POST['icon'];
-
-        // sql query for insert data
-        $sql = "UPDATE danhmuc SET 
-        ten_danhmuc = '$ten_danhmuc',
-        mau_nen = '$mau_nen', 
-        icon = '$icon' WHERE ma_danhmuc = $ma_danhmuc";
-        pdo_execute($sql);
-        echo "<script>
-            window.location.href='index.php?url=danh_sach_danh_muc';
-        </script>";
-        // if (pdo_execute($sql)) {
-
-        //     header("Location:index.php?url=danh_sach_danh_muc");
-        // }
-    }
+    $sql = "UPDATE danhmuc SET ten_danhmuc = '$ten_danhmuc', mau_nen = '$mau_nen', icon = '$icon' WHERE ma_danhmuc = $ma_danhmuc";
+    pdo_execute($sql);
 }
